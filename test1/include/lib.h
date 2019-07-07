ac3f0ed01d8717a4d5cae581f7b636f29ea89b47
chenysh <chenysh@yonyou.com>
2019-07-07 23:29:59 +0800

    cmake

---
+++ test1/include/lib.h
@@ -0,0 +1,5 @@
+#ifndef __LIB_H__
+
+int add(int a, int b);
+
+#endif
---
+++ test1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <lib.h>
+
+int main()
+{
+    printf("1+2=%d\n", add(1, 2));
+    return 0;
+}
